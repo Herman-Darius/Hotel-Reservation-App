f54affef327b940288724dddd9f3fb873a58c084
Herman Darius-Razvan <darius.herman21@yahoo.com>
2024-05-17 16:39:58 +0300

    Internship Siemens test

---
+++ HotelReservationApp/src/main/target/classes/database.sql
@@ -0,0 +1,4 @@
+INSERT INTO hotel (id, name, latitude, longitude) VALUES (1, 'Hotel Hotii de Cai', 46.764654252624204, 23.598674125224626);
+INSERT INTO room (id, room_number, type, price, is_available, hotel_id) VALUES (1, 210, 2, 200, true, 1);
+INSERT INTO room (id, room_number, type, price, is_available, hotel_id) VALUES (2, 125, 1, 350, true, 1);
+INSERT INTO room (id, room_number, type, price, is_available, hotel_id) VALUES (3, 87, 1, 300, false, 1);
